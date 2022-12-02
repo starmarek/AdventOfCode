bf9019f2a4a6791e39483c60da8d85114ba1b1e4
Aleksander Pucher <pucher.aleksander@gmail.com>
2022-12-02 20:47:10 +0100

    day 1

---
+++ 1/1.py
@@ -0,0 +1,16 @@
+with open("input.txt") as f:
+    c = 0
+    calories = []
+    for line in f.read().splitlines():
+        if not line:
+            calories.append(c)
+            c = 0
+            continue
+        line = int(line)
+        c += line
+    calories.append(c)
+    calories.sort(reverse=True)
+        
+
+print(max(calories)) # first star
+print(sum(calories[:3]))  # second star
