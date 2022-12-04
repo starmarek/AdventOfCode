b621ce9908170c053d43ce79ae81ba30cbbdc865
Aleksander Pucher <pucher.aleksander@gmail.com>
2022-12-04 15:11:28 +0100

    day 4

---
+++ 2022/4/4.py
@@ -0,0 +1,23 @@
+with open("input.txt") as f:
+    l = []
+    for line in f.read().splitlines():
+        elf1, elf2 = line.split(",")
+        elf1_range1, elf1_range2 = list(map(int, elf1.split("-")))
+        elf2_range1, elf2_range2 = list(map(int, elf2.split("-")))
+        elf1_range = list(range(elf1_range1, elf1_range2 + 1))
+        elf2_range = list(range(elf2_range1, elf2_range2 + 1))
+
+        l.append([elf1_range, elf2_range])
+
+c = 0
+c2 = 0
+for pair in l:
+    elf1 = set(pair[0])
+    elf2 = set(pair[1])
+
+    if elf1.issubset(elf2) or elf2.issubset(elf1):
+        c += 1
+    if elf1 & elf2:
+        c2 += 1
+print(c) # first_star
+print(c2) # second_star
