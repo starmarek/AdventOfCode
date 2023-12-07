c4ce2a8829f6da3d010762a5dbd98d03ff8c418b
Aleksander Pucher <pucher.aleksander@gmail.com>
2023-12-07 23:41:02 +0100

    solve day 2023/07

--- 2023/07/07.py
+++ 2023/07/07.py
@@ -1,42 +1,30 @@
 import bisect
-from functools import cmp_to_key
 
-with open("input.txt") as f:
-    data = [line.split() for line in f]
-
-trans_table = str.maketrans("TJQKA", "ABCDE")
-
-
-def comp(line1, line2):
-    hand1, _ = line1
-    hand2, _ = line2
-
-    hand1, hand2 = hand1.translate(trans_table), hand2.translate(trans_table)
-    type_hand1, type_hand2 = calc_type(hand1), calc_type(hand2)
-
-    if type_hand1 > type_hand2:
-        return 1
-    elif type_hand1 < type_hand2:
-        return -1
-
-    for card1, card2 in zip(hand1, hand2):
-        ord1, ord2 = ord(card1), ord(card2)
-        if ord1 > ord2:
-            return 1
-        elif ord1 < ord2:
-            return -1
-        else:
-            continue
-    return 0
+trans_table1 = str.maketrans("TJQKA", "ABCDE")
+trans_table2 = str.maketrans("TJQKA", "A0CDE")
 
 
 def calc_type(hand):
     return sum(map(hand.count, hand))
 
 
-values = []
-for l in data:
-    bisect.insort(values, l, key=cmp_to_key(comp))
+with open("input.txt") as f:
+    data1 = []
+    for line in f:
+        hand, bid = line.split()
+        hand = hand.translate(trans_table1)
+        bisect.insort(data1, (calc_type(hand), hand, int(bid)))
+
+with open("input.txt") as f:
+    data2 = []
+    for line in f:
+        hand, bid = line.split()
+        hand = hand.translate(trans_table2)
+        bisect.insort(
+            data2,
+            (max(calc_type(hand.replace("0", r)) for r in hand), hand, int(bid)),
+        )
 
 
-print(sum(i * int(d[1]) for i, d in enumerate(values, 1)))  # first star
+print(sum(i * d[2] for i, d in enumerate(data1, 1)))  # first star
+print(sum(i * d[2] for i, d in enumerate(data2, 1)))  # second star
